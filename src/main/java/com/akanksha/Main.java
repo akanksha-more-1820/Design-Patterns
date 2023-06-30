5b4a596f285a102680d5f5b1ba75cd54ae4ca023
akanksha-more-1820 <akankshamore1820@gmail.com>
2023-06-30 23:36:07 +0530

    am-01 changing main

--- src/main/java/com/akanksha/Main.java
+++ src/main/java/com/akanksha/Main.java
@@ -19,30 +19,30 @@ import com.akanksha.strategy.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
-//        int score=11;
-//        UniversityFactory universityFactory=UniversityFactory.getUniversityFactory(score);
-//        System.out.println(universityFactory.getAdmitCard("Physics"));
-//        System.out.println(universityFactory.getFeeCalculator("Math").calculateFee());
-
+        int score=11;
+        UniversityFactory universityFactory=UniversityFactory.getUniversityFactory(score);
+        System.out.println(universityFactory.getAdmitCard("Physics"));
+        System.out.println(universityFactory.getFeeCalculator("Math").calculateFee());
+//
 //        ShapeFactory shapeFactory= (ShapeFactory) new ShapeFactory().getShape("Circle");
 //        Shape shape=shapeFactory.getShape("Circle");
 //        shape.draw();
 
-//        Shape shape=new ShapeFactory().getShape("Circle");
-//        shape.draw();
+        Shape shape=new ShapeFactory().getShape("Circle");
+        shape.draw();
 
-//        Vehicle vehicle=new SportVehicle();
-//        vehicle.drive();
+        Vehicle vehicle=new SportVehicle();
+        vehicle.drive();
 
-//        IphoneObservalbleImpl iphoneObservalble=new IphoneObservalbleImpl();
-//
-//        NotificationAlertObserver obs1=new EmailAlertObserverImpl("asd",iphoneObservalble);
-//        NotificationAlertObserver obs2=new MobileAlertObserverImpl("fgh",iphoneObservalble);
-//
-//        iphoneObservalble.add(obs1);
-//        iphoneObservalble.add(obs2);
-//
-//        iphoneObservalble.setStockCount(1);
+        IphoneObservalbleImpl iphoneObservalble=new IphoneObservalbleImpl();
+
+        NotificationAlertObserver obs1=new EmailAlertObserverImpl("asd",iphoneObservalble);
+        NotificationAlertObserver obs2=new MobileAlertObserverImpl("fgh",iphoneObservalble);
+
+        iphoneObservalble.add(obs1);
+        iphoneObservalble.add(obs2);
+
+        iphoneObservalble.setStockCount(1);
 
 
         BasePizza pizza=new ExtraCheese(new ExtraChicken(new Chicken()));
